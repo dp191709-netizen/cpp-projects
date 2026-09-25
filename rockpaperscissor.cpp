e3845fd333b255fac883ac387730140cb1a94da2
dp191709-netizen <dp191709@gmail.com>
2026-09-25 10:37:33 +0530

    Add files via upload

---
+++ rockpaperscissor.cpp
@@ -0,0 +1,33 @@
+// #include<iostream>
+// #include <cstdlib>
+// #include<ctime>
+// using namespace std;
+// int main(){
+//     srand(time(0));
+//     printf("rock paper scissor\n");
+//     printf("1)rock\n");
+//     printf("2) paper \n");
+//     printf("3) scissor\n");
+//     int x;
+//     cout<<"enter your number \n";
+//     cin>>x;
+//     if (x==1){printf("u choose rock\n");}
+//    else if (x==2){printf("u choose paper\n");}
+//    else  if (x==3){printf("u choose scissor\n");}
+// int y;
+// y=rand()%3+1;
+//  if (y==1){printf("computer choose rock\n");}
+//    else if (y==2){printf("computer choose paper\n");}
+//    else  if (y==3){printf("computer choose scissor\n");}
+
+// if(x==y){printf("draw");}
+// if(x==1&&y==2)printf("computer won");
+// else if(x==1&&y==3)printf("you won");
+// else if(x==2&&y==1)printf("you won");
+// else if(x==2&&y==3)printf("computer won");
+// else if(x==3&&y==2)printf("you won");
+// else if(x==3&&y==1)printf("computer won");
+
+// return 0;
+
+// }
